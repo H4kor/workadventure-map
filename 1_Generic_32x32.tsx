5e97a8445b8f9ad20d88a5156eed2f24a6457651
H4kor <niko@rerere.org>
2020-12-07 20:02:01 +0000

    Deploying to gh-pages from master @ dea4ba60ad5768797b61a6c88e250d5a48ca30c8 🚀

---
+++ 1_Generic_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="1_Generic_32x32" tilewidth="32" tileheight="32" tilecount="880" columns="16">
+ <image source="tilesets/1_Tilesets/32x32/Theme_Sorter_32x32/1_Generic_32x32.png" width="512" height="1760"/>
+</tileset>
